ab964be5e16fb78f96b5ee8ee2c245aaa3685737
Alexander Kolesnikov <kolochik72ru@gmail.com>
2024-12-24 20:49:04 +0500

    hope-it-is-final

--- parcel.go
+++ parcel.go
@@ -78,6 +78,7 @@ func (s ParcelStore) SetStatus(number int, status string) error {
 	return err
 }
 
+// comment
 func (s ParcelStore) SetAddress(number int, address string) error {
 	_, err := s.db.Exec("UPDATE parcel SET address = :address WHERE number = :number AND status = :status",
 		sql.Named("address", address),
